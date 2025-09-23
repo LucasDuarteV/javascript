fd7417fd73b43e65310aee64453761615643a0cb
LucasDuarteV <lucasdv2733@gmail.com>
2025-09-23 09:59:52 -0300

    condições em javascript

---
+++ aula011/ex008.js
@@ -0,0 +1,10 @@
+var velocidade = 50
+
+console.log(`Essa é a sua velociade na via ${velocidade} km|h`)
+
+if (velocidade > 60){
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!!!')
+}
+
+
+console.log('Dirija sempre com o cinto de segurança!')
