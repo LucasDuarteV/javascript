918a72e1c4cd44a94ce06e43a3e8425eddd51673
LucasDuarteV <lucasdv2733@gmail.com>
2025-09-25 13:50:23 -0300

    variáveis compostas

---
+++ aula015/ambiente.js
@@ -0,0 +1,6 @@
+let num = [6 , 2 , 3 , 4 , 5]
+num.push(1)
+num.sort()
+console.log(num[0])
+console.log(`Tem ${num.length} vetores.`)
+console.log(`Nosso vetor é ${num}`)
---
+++ aula015/vetornatela.js
@@ -0,0 +1,5 @@
+let valores = [1,2,3,4,5,6,7]
+
+for (let pos in valores){
+    console.log(`Os vetores nas posições ${pos} teram os seguintes valores ${valores[pos]}`)
+}
